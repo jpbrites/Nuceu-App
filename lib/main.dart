7883d4abf0251e57669939a1012fcd8f091eb124
EduardoOPR <67355426+EduardoOPR@users.noreply.github.com>
2022-07-25 15:02:34 -0300

    firebase criado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nuceu/view/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: 'AIzaSyC-6MXlDkcAdZTXwEOQl3HnSzeSaxCym_s',
+        appId: '1:882302187340:android:1d79d3577052fadc9a01e9',
+        messagingSenderId: '882302187340',
+        projectId: 'univasf-nuceu-firebase'),
+  );
   runApp(const MyApp());
 }
 
