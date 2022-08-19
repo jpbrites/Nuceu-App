ce7c451fce296fb2b5be853ce0897116b6203e7e
EduardoOPR <67355426+EduardoOPR@users.noreply.github.com>
2022-08-19 16:11:28 -0300

    novas opções no drawer

--- lib/view/widgets/navigation_drawer.dart
+++ lib/view/widgets/navigation_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:nuceu/themes/themes.dart';
+import 'package:nuceu/utils/dialogs/addEventoPopup.dart';
 
 class NavigationDrawer extends StatefulWidget {
   const NavigationDrawer({Key? key}) : super(key: key);
@@ -19,41 +20,70 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              if (!isLogged)
-                Padding(
-                  padding:
-                      EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-                  child: ListTile(
-                    leading: const Icon(
-                      Icons.login,
-                    ),
-                    title: Text(
-                      'Realizar login',
-                      style: Themes.latoLight(17),
-                    ),
-                    onTap: () =>
-                        Navigator.of(context).pushNamed('/login-screen'),
+              Padding(
+                padding:
+                    EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.home,
                   ),
+                  title: Text(
+                    'Tela inicial',
+                    style: Themes.latoLight(17),
+                  ),
+                  onTap: () => Navigator.of(context).pushNamed('/'),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.play_circle,
+                ),
+                title: Text(
+                  'Videos informativos',
+                  style: Themes.latoLight(17),
+                ),
+                onTap: () => Navigator.of(context).pushNamed('/videos-screen'),
+              ),
+              if (!isLogged)
+                ListTile(
+                  leading: const Icon(
+                    Icons.login,
+                  ),
+                  title: Text(
+                    'Realizar login',
+                    style: Themes.latoLight(17),
+                  ),
+                  onTap: () => Navigator.of(context).pushNamed('/login-screen'),
                 ),
               if (isLogged)
-                Padding(
-                  padding:
-                      EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-                  child: ListTile(
-                    leading: const Icon(
-                      Icons.logout,
-                    ),
-                    title: Text(
-                      'Sair da conta',
-                      style: Themes.latoLight(17),
-                    ),
-                    onTap: () {
-                      FirebaseAuth.instance
-                          .signOut()
-                          .then((value) => {Navigator.pushNamed(context, '/')});
-                    },
+                ListTile(
+                  leading: const Icon(
+                    Icons.library_add,
                   ),
-                )
+                  title: Text(
+                    'Adicionar evento',
+                    style: Themes.latoLight(17),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, '/edit-post-screen');
+                  },
+                ),
+              if (isLogged)
+                ListTile(
+                  leading: const Icon(
+                    Icons.logout,
+                  ),
+                  title: Text(
+                    'Sair da conta',
+                    style: Themes.latoLight(17),
+                  ),
+                  onTap: () {
+                    FirebaseAuth.instance
+                        .signOut()
+                        .then((value) => {Navigator.pushNamed(context, '/')});
+                  },
+                ),
             ],
           ),
         ),
