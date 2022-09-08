a35bb02a3838e50f0000c622c6155f457e42f8db
JP-BRITES <74506182+JP-BRITES@users.noreply.github.com>
2022-09-07 21:37:34 -0300

    Modificação do popup de edição do evento

--- lib/utils/dialogs/admin_event_dialog.dart
+++ lib/utils/dialogs/admin_event_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nuceu/themes/themes.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
 
 editEventDialoig({
   required BuildContext context,
@@ -26,105 +27,105 @@ editEventDialoig({
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text('O que você deseja?',
-                  textAlign: TextAlign.center, style: Themes.latoRegular(20)),
-              const SizedBox(height: 15),
               Column(
                 //mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  InkWell(
+                 Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: InkWell(
                     onTap: onView,
                     child: Container(
-                      width: double.infinity,
-                      height: 45,
+                      width: 240,
+                      height: 47,
                       decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Color.fromARGB(255, 0, 0, 0)),
                         color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.75),
-                            blurRadius: 4,
-                            offset: const Offset(0, 4),
-                          ),
-                        ],
                       ),
-                      child: Center(
-                        child: Text(
+                      child: Row(
+                        children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 20, right: 10, top: 0, bottom: 0),
+                          child: Icon(
+                            Icons.remove_red_eye,
+                            color: Color(0xFF535353),
+                            size: 24,
+                          ),
+                        ),
+                        Text(
                           'Vizualizar',
                           style: Themes.latoRegular(20),
                         ),
+                        ]
                       ),
                     ),
                   ),
-                  InkWell(
+                 ),
+
+                  Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: InkWell(
                     onTap: onEdit,
                     child: Container(
-                      width: double.infinity,
-                      height: 45,
+                      width: 240,
+                      height: 47,
                       decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Color.fromARGB(255, 0, 0, 0)),
                         color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.75),
-                            blurRadius: 4,
-                            offset: const Offset(0, 4),
-                          ),
-                        ],
+                        
                       ),
-                      child: Center(
-                        child: Text(
+                      child: Row(
+                        children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 20, right: 10, top: 0, bottom: 0),
+                          child: Icon(
+                            Icons.edit,
+                            color: Color(0xFF535353),
+                            size: 24,
+                          ),
+                        ),
+                        Text(
                           'Editar',
                           style: Themes.latoRegular(20),
                         ),
+                        ]
                       ),
                     ),
                   ),
-                  InkWell(
+                  ),
+
+                  Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 20),
+                  child: InkWell(
                     onTap: onDelete,
                     child: Container(
-                      width: double.infinity,
-                      height: 45,
+                      width: 240,
+                      height: 47,
                       decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Color.fromARGB(255, 0, 0, 0)),
                         color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.75),
-                            blurRadius: 4,
-                            offset: const Offset(0, 4),
-                          ),
-                        ],
+                        
                       ),
-                      child: Center(
-                        child: Text(
-                          'Excluir',
-                          style: Themes.latoRegular(20),
+                      child: Row(
+                        children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 20, right: 10, top: 0, bottom: 0),
+                          child: Icon(
+                            Icons.delete,
+                            color: Color(0xFFFB383C),
+                            size: 24,
+                          ),
                         ),
+                        Text(
+                          'Excluir',
+                          style: Themes.latoRegular(20).copyWith(color: Color(0xFFFB383C)),
+                        ),
+                        ]
                       ),
                     ),
                   ),
-                  const SizedBox(width: 10),
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      width: double.infinity,
-                      height: 45,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius:
-                            BorderRadius.vertical(bottom: Radius.circular(10)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.75),
-                            blurRadius: 4,
-                            offset: const Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: Center(
-                        child: Text('Voltar', style: Themes.latoRegular(20)),
-                      ),
-                    ),
                   ),
                 ],
               ),
